cdd5eb4d7e84c9eb8af525dc9d0f7bb21028178e
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-06-07 16:00:16 +0200

    Add Watson SDK as Dependency

---
+++ Sources/Credentials.swift
@@ -0,0 +1,9 @@
+//
+//  Credentials.swift
+//  ToneAnalyzer-iOS
+//
+//  Created by Thomas Paul Mann on 07/06/16.
+//  Copyright © 2016 Thomas Paul Mann. All rights reserved.
+//
+
+import Foundation
