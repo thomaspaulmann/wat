1e28656b0dd6f597a827f452161159897549f2ae
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-06-11 09:47:46 +0200

    Add templates for all services

---
+++ Sources/DialogViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DialogViewController.swift
+//  Wat
+//
+//  Created by Thomas Paul Mann on 11/06/16.
+//  Copyright © 2016 Thomas Paul Mann. All rights reserved.
+//
+
+import UIKit
+
+class DialogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
