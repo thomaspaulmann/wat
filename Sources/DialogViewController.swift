ddbe727f61f7b0d56defdafdc1f859a6c39a6416
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-06-11 11:25:39 +0200

    [WIP] Add pizza example for dialog service

--- Sources/DialogViewController.swift
+++ Sources/DialogViewController.swift
@@ -7,29 +7,77 @@
 //
 
 import UIKit
+import DialogV1
 
 class DialogViewController: UIViewController {
+
+    // TODO: Extract hard coded strings
+    // TODO: Add chat interface for communication
+
+    // MARK: - Properties
+
+    private var dialog: Dialog?
+    private var dialogId: DialogID?
+    private var conversationID: Int?
+    private var clientID: Int?
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        dialog = Dialog(username: Credentials.dialogUsername, password: Credentials.dialogPassword)
+
+        guard let fileURL = loadDialogFile("pizza", withExtension: "xml") else {
+            showAlert(withText: "Could not load Dialog File!")
+            return
+        }
+
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+
+        if let id = userDefaults.objectForKey("dialog-id") as? DialogID {
+            dialogId = id
+            print("Dialog loaded with ID \(dialogId)")
+        } else {
+            dialog?.createDialog("wat",
+                                 fileURL: fileURL,
+                                 failure: { [weak self] (error) in
+                                    self?.showAlert()
+                                    print(error)
+                },
+                                 success: { [weak self] (dialogId) in
+                                    userDefaults.setValue(dialogId, forKey: "dialog-id")
+                                    self?.dialogId = dialogId
+                                    print("New dialog created with ID \(dialogId)")
+                })
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let failure = { (error: NSError) in print(error) }
+        dialog!.converse(dialogId!,
+                        failure: failure) { [weak self] conversationResponse in
+                            // save conversation parameters
+                            self?.conversationID = conversationResponse.conversationID
+                            self?.clientID = conversationResponse.clientID
+                            
+                            // print message from Watson
+                            print(conversationResponse.response)
+        }
     }
-    */
+
+    // MARK: - File Access
+
+    func loadDialogFile(name: String, withExtension: String) -> NSURL? {
+        let bundle = NSBundle(forClass: self.dynamicType)
+
+        guard let url = bundle.URLForResource(name, withExtension: withExtension) else {
+            return nil
+        }
+
+        return url
+    }
 
 }
