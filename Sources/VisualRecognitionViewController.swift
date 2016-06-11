bdc5e57c5ded1647c15bcde50c7cb336051720c0
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-06-11 12:45:21 +0200

    Add basics to use visual recognition

--- Sources/VisualRecognitionViewController.swift
+++ Sources/VisualRecognitionViewController.swift
@@ -7,29 +7,26 @@
 //
 
 import UIKit
+import VisualRecognitionV3
 
 class VisualRecognitionViewController: UIViewController {
+
+    // MARK: - Properties
+
+    private var visualRecogntion: VisualRecognition?
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        visualRecogntion = VisualRecognition(apiKey: Credentials.visualRecognitionApiKey, version: Credentials.visualRecognitionVersionDate)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let failure = { (error: NSError) in print(error) }
+        visualRecogntion?.detectFaces("https://upload.wikimedia.org/wikipedia/commons/f/fc/Ryan_Gosling_2_Cannes_2011_(cropped).jpg",
+                                      failure: failure) { imagesWithFaces in
+            print(imagesWithFaces)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
