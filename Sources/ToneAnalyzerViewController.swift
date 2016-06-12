68e0cdf20cf05d5e244f49bca1b54d49888a7660
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-06-12 12:43:37 +0200

    Add simple implementation of ToneAnalyzer

--- Sources/ToneAnalyzerViewController.swift
+++ Sources/ToneAnalyzerViewController.swift
@@ -7,31 +7,20 @@
 //
 
 import UIKit
+import ToneAnalyzerV3
 
 class ToneAnalyzerViewController: UIViewController {
 
-    // TODO: tbc
+    // MARK: - Properties
+
+    private var toneAnalyzer: ToneAnalyzer?
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        toneAnalyzer = ToneAnalyzer(username: Credentials.toneAnalyzerUsername, password: Credentials.toneAnalyzerPassword, version: Credentials.toneAnalyzerVersionDate)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
