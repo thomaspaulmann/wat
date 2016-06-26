fe6618087a278b468d4504728bd9ccea158ea961
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-06-26 13:34:26 +0200

    Make Color Scheme Creators static for easy access

--- Sources/UIColor.swift
+++ Sources/UIColor.swift
@@ -12,35 +12,35 @@ extension UIColor {
 
     // MARK: - Text Colors
 
-    func textColor() -> UIColor {
+    static func textColor() -> UIColor {
         return UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.00)
     }
 
-    func secondaryTextColor() -> UIColor {
+    static func secondaryTextColor() -> UIColor {
         return UIColor(red:0.69, green:0.69, blue:0.69, alpha:1.00)
     }
 
     // MARK: - Background Colors
 
-    func backgroundColor() -> UIColor {
+    static func backgroundColor() -> UIColor {
         return UIColor(red:0.09, green:0.14, blue:0.20, alpha:1.00)
     }
 
-    func lighterBackgroundColor() -> UIColor {
+    static func lighterBackgroundColor() -> UIColor {
         return UIColor(red:0.15, green:0.19, blue:0.25, alpha:1.00)
     }
 
     // MARK: - Other Colors
 
-    func accentColor() -> UIColor {
+    static func accentColor() -> UIColor {
         return UIColor(red:0.88, green:0.93, blue:0.00, alpha:1.00)
     }
 
-    func hightlightColor() -> UIColor {
+    static func hightlightColor() -> UIColor {
         return UIColor(red:0.42, green:0.55, blue:0.64, alpha:1.00)
     }
 
-    func errorColor() -> UIColor {
+    static func errorColor() -> UIColor {
         return UIColor(red:0.91, green:0.00, blue:0.23, alpha:1.00)
     }
 
